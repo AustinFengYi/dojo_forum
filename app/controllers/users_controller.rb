e511943270d2b91a7224d483cc510e56023e2563
xs1523c <xs1523c@gmail.com>
2018-11-07 19:03:26 +0800

    set_posts+comments+collects_drafts_controller & create_friends_controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+
+  def posts
+    @user = User.find(params[:id])
+    @posts = @user.posts.where(status: true).order(created_at: :desc)
+  end
+
+  def comments
+    @user = User.find(params[:id])
+    @replies = @user.replies.order(created_at: :desc)
+  end
+
+  def collects
+    @user = User.find(params[:id])
+    @collects = @user.collects.order(created_at: :desc)
+  end
+
+  def drafts
+    @user = User.find(params[:id])
+    @posts = @user.posts.where(status: false).order(created_at: :desc)
+  end
+
+  def friends
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :avatar, :introduction)
+  end
+
+end
+
