ba1c89c17eada2acd0e49109389df8d8bf3ec915
xs1523c <xs1523c@gmail.com>
2018-11-08 17:27:57 +0800

    build_join_table_post_category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_08_061902) do
+ActiveRecord::Schema.define(version: 2018_11_08_092545) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 2018_11_08_061902) do
     t.integer "user_id"
     t.integer "friend_id"
     t.boolean "status", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_categories", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
