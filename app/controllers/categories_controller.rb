ddbd45b2cefee289439aac0d5198bafbe585c2d8
xs1523c <xs1523c@gmail.com>
2018-11-08 14:50:30 +0800

    adjust_index_count_pos

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,8 @@ class CategoriesController < ApplicationController
   def show
     @categories = Category.all
     @category = Category.find(params[:id])
-    @posts = @category.posts.where(status:true).order(created_at: :desc).page(params[:page]).per(10)
+    #@posts = @category.posts.where(status:true).order(created_at: :desc).page(params[:page]).per(10)
+    @q = @category.posts.where(status: true).ransack(params[:q])
+    @posts = @q.result.order(id: :asc).page(params[:page]).per(10)
   end
 end
